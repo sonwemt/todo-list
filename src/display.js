147150f012181330e1ded6c4af78fe8b64333fea
Sondre <sonfjell@gmail.com>
2022-06-19 02:53:34 +0200

    Clean up clutter/inconsistensies in DOM scripts

--- src/display.js
+++ src/display.js
@@ -4,10 +4,11 @@ export default class DisplayController {
     }
     #createPageBody () {
         const wrapper = document.getElementById('content');
+
         const header = document.createElement('div');
-        const headerText = document.createTextNode('To Do List');
         header.classList.add('header');
-        header.appendChild(headerText);
+        header.textContent = 'To Do List';
+        
         
 
         const projectBody = document.createElement('div');
@@ -34,18 +35,23 @@ export default class DisplayController {
             const card = document.createElement('div');
             card.classList.add(`project`);
             card.id = i;
+            
             const addButton = document.createElement('button');
             addButton.textContent = 'Add Task'; 
-            addButton.id = i;
             addButton.classList.add('addTask');
+            addButton.id = i;
+            
             const cardContent = document.createElement('div');
+            cardContent.classList.add('items');
+            cardContent.id = i;
+
             const projectInfo = projects.getProject(i);
             const projectName = document.createElement('div');
             projectName.textContent = projectInfo.name;
+            projectName.classList.add('projectTitle');
             card.appendChild(projectName);
             
-            cardContent.id = i;
-            cardContent.classList.add('items');
+            
     
             for(let x = 0; x < projectInfo.NumberOfItems; x++) {
                 const item = document.createElement('div');
@@ -56,6 +62,7 @@ export default class DisplayController {
                 removeItem.textContent = 'Remove';
                 removeItem.classList.add(`removeItem`);
                 removeItem.id = x;
+
                 cardContent.appendChild(removeItem);
 
             }
